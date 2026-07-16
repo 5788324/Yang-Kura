8631aa78374594cdefd64ab145f4ea4068bbf0e4
5788324 <121728123+5788324@users.noreply.github.com>
2026-07-16 20:23:16 +0800

    feat: export design-system primitives

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,10 @@
+export { Button, type ButtonProps, type ButtonSize, type ButtonVariant } from './Button';
+export { Dialog, type DialogProps } from './Dialog';
+export { Feedback, type FeedbackProps, type FeedbackTone } from './Feedback';
+export {
+  Surface,
+  type SurfaceElevation,
+  type SurfacePadding,
+  type SurfaceProps,
+  type SurfaceTone,
+} from './Surface';
