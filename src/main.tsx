0afb5a3cd58509c7efe0b9f3be1cfe2047718b3f
5788324 <121728123+5788324@users.noreply.github.com>
2026-07-16 20:23:29 +0800

    feat: load semantic design tokens

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import {installRuntimeAccessibility} from './runtimeAccessibility';
+import './styles/design-tokens.css';
 import './index.css';
 import './accessibility.css';
 import './playerThemeBridge.css';
